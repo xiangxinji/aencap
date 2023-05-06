9f1842c5675707a8f5aa8807cab4ab26113dec3e
向歆纪 <xxjbbking@163.com>
2023-05-06 15:23:00 +0800

    ff

--- src/hooks/modules/logic/usePageCommand.ts
+++ src/hooks/modules/logic/usePageCommand.ts
@@ -3,21 +3,28 @@ import { onUnmounted, watch } from "vue";
 const KeyboardCommands = {
   // ctrl + s 保存事件
   save: function (event: KeyboardEvent, callback: Function) {
-    if ((event.ctrlKey && event.key === "s") || (event.metaKey && event.key === "s")) {
+    if (
+      (event.ctrlKey && event.key === "s") ||
+      (event.metaKey && event.key === "s")
+    ) {
       event.preventDefault(); // Prevent the default behavior of saving the webpage
       callback();
     }
   },
 };
 
-export function usePageCommand(type: keyof typeof KeyboardCommands, watchGetter: () => boolean, callback: Function) {
+export function usePageCommand(
+  type: keyof typeof KeyboardCommands,
+  watchGetter: () => boolean,
+  callback: Function
+) {
   const cb = (event: KeyboardEvent) => {
     const handler = KeyboardCommands[type];
     if (handler) handler(event, callback);
   };
   watch(
     watchGetter,
-    (v) => {
+    (v: any) => {
       if (v) {
         document.addEventListener("keydown", cb);
       } else {
