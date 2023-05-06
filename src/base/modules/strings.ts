e31572f531f31906eda779fe03a2dc40aa7886fd
向歆纪 <xxjbbking@163.com>
2023-05-06 15:09:01 +0800

    fix : 修复 splitIgnoreQuotes 问题

--- src/base/modules/strings.ts
+++ src/base/modules/strings.ts
@@ -14,6 +14,7 @@ export function splitIgnoreQuotes(str: string, separator = ",") {
     const char = str[i];
     if (char === '"' || char === "'") {
       inQuote = !inQuote;
+      current += char;
     } else if (char === separator && !inQuote) {
       result.push(current);
       current = "";
